22db9fee38681af40a91a3b1b1054bd138605dc2
JCSR <johnconnorsryan@gmail.com>
2020-04-26 19:19:07 -0400

    finish the set and get for previous user searches, dynamically display history in list

---
+++ README.md
@@ -0,0 +1,7 @@
+The open weather API allows this site to pull various data from a daily and five day forcast. By typing a city name the information is requested from the API endpoints and displayed to the user. Cities previously searched for are saved to local storage and placed into a drop down list for user reference. 
+The built in breakpoints in the bootstrap lobrary let the site automatically adjust its column layout depending on the user's screen size. The API information is called out for each element directly, something that can be shortened with a for loop and a biut more time. 
+The real time date information was pulled from the open weather API but configured using moment.js to better fit the page layout. 
+Vanilla CSS was used to create borders and an on hover effect for the daily forcast.
+
+
+![](2020-04-26-19-04-04.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//variables for the daily weather function
 var buttonEl = document.querySelector('.button');
 var inputValueEl = document.querySelector('.inputValue');
  var nameEl= document.querySelector('.name');
@@ -6,33 +7,43 @@ var inputValueEl = document.querySelector('.inputValue');
  var tempEl= document.querySelector('.temp');
  var humidityEl= document.querySelector('.humidity');
  var windspeedEl = document.querySelector('.windspeed');
-
+//variables for the five day date function
  var date1El=document.querySelector('.date1');
  var date2El=document.querySelector('.date2');
  var date3El=document.querySelector('.date3');
  var date4El=document.querySelector('.date4');
  var date5El=document.querySelector('.date5');
-
+//var for the five day images
  const image1=document.getElementById('wicon1');
  const image2=document.getElementById('wicon2');
  const image3=document.getElementById('wicon3');
  const image4=document.getElementById('wicon4');
  const image5=document.getElementById('wicon5');
-
-
+//var for the five day temperatures
  var temp1El=document.querySelector('.temp1');
  var temp2El=document.querySelector('.temp2');
  var temp3El=document.querySelector('.temp3');
  var temp4El=document.querySelector('.temp4');
  var temp5El=document.querySelector('.temp5');
-
+//var for the five day humidity
  var humidity1El=document.querySelector('.humidity1');
  var humidity2El=document.querySelector('.humidity2');
  var humidity3El=document.querySelector('.humidity3');
  var humidity4El=document.querySelector('.humidity4');
  var humidity5El=document.querySelector('.humidity5');
- 
+ //setting user input to an array in local storage
+ var setLocalStorage = [];
+ if (localStorage.getItem('nameValue') === null) {
+        console.log('empty');
+        setLocalStorage = []
+         
+    } else {
+        console.log('not empty');
+        setLocalStorage = JSON.parse( localStorage.getItem('nameValue') )
+    }
 
+//initial function used for fetching the daily information from the API and dynamically    
+//setting the elements to the page
 function buttonCall() {
     
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValueEl.value + '&appid=9258310cbe3ce12fc60025bf8d0e566b' + '&units=imperial')
@@ -40,7 +51,7 @@ function buttonCall() {
       return response.json();
     })
     .then((data) => {
-       // console.log(data)
+  //vars for the information pulled from the openweather object
       var nameValue = data['name'];
       var tempValue = data['main']['temp'];
       var desValue = data['weather'][0]['description'];
@@ -49,18 +60,24 @@ function buttonCall() {
       var iconValue = data['weather'][0]['icon'];
  
       
-    
+    //setting that information to the html global vars assigned above
         nameEl.innerHTML = nameValue;
         descriptionEl.innerHTML = 'Description: ' + desValue;
         iconEl.setAttribute("src","https://openweathermap.org/img/wn/" + iconValue + "@2x.png");
         tempEl.innerHTML = 'Temp: ' + tempValue + 'F';
         humidityEl.innerHTML = 'Humidity: ' + humidityValue;
         windspeedEl.innerHTML = 'Windspeed: ' + windspeedValue;
-     // .catch(err => alert('Pleae select a valid city.'))
+
+    //pushing the nameValue given by the user into an array.
+        setLocalStorage.push(nameValue)
+        localStorage.setItem('nameValue', JSON.stringify(setLocalStorage) )
     });
 
 };
-
+//function that pulls information and displays to the page just like the above
+//function but this does it for 5x the amount of information, I could use a 
+//for loop to iterate over the information and shorten the code's repetition
+//just ran out of time.
 function fiveDay() {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValueEl.value + '&appid=9258310cbe3ce12fc60025bf8d0e566b' + '&units=imperial')
     .then((response) => {
@@ -118,35 +135,17 @@ function fiveDay() {
     })
 };
 
-const myForm = document.getElementById("todo");
-const list = document.querySelector(".list-items");
 
-
-function addTodo(e) {
- // e.preventDefault();
-  const userInput = document.querySelector(".userAdd").value;
-  const userListItem = document.createElement("li");
-  userListItem.appendChild(document.createTextNode(userInput));
-  list.appendChild(userListItem);
-  const myArray = map(listItems, getText);
-  localStorage.setItem('userListItem', JSON.stringify(myArray));
+//takes the information in localstorage and apppends it to the element <li>
+//dynamically creates a list of previous search names on the page.
+ function previousSearches() {
+    for (let i = 0; i < setLocalStorage.length; i++) {
+        console.log(setLocalStorage[i]);
+        var previousSerchEl = $('<li>'); //creating an element
+        previousSerchEl.text( setLocalStorage[i] ) //modify
+        $('.list-items').append(previousSerchEl)
+    }
+    
 }
-
-const listItems = document.getElementsByTagName('li');
-
-
-function map(arrayLike, fn) {
-    var ret = [], i = -1, len = arrayLike.length;
-    while (++i < len) ret[i] = fn(arrayLike[i]);
-    return ret;
-}
-
-
-function getText(node) {
-    if (node.nodeType === 3) return node.data;
-    var txt = '';
-    if (node = node.firstChild) do {
-        txt += getText(node);
-    } while (node = node.nextSibling);
-    return txt;
-}
+//calling the function on page load to display any pages already set to local
+previousSearches()
